34244f6d4b1a60fd81720170edbe9390c68f97a4
carl <carl@clipboard.app>
2023-04-01 08:39:39 +1100

    add tsserver to only use one server

--- config.lua
+++ config.lua
@@ -123,9 +123,12 @@ lvim.builtin.treesitter.highlight.enable = true
 
 -- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
 -- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "tsserver" })
+local opts = {
+  root_dir = require('lspconfig.util').root_pattern('.git')
+}
+require("lvim.lsp.manager").setup("tsserver", opts)
+--
 
 -- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
 -- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
@@ -193,7 +196,6 @@ lvim.plugins = {
       })
     end
   },
-  { "catppuccin/nvim" },
   {
     "zbirenbaum/copilot.lua",
     event = { "VimEnter" },
