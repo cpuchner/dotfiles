f97abe168aa6c44f15cd100974355af1dd8818dd
carl <carl.puchner@gmail.com>
2025-03-30 19:34:10 +1100

    remove old thing

--- nvim/config.lua
+++ nvim/config.lua
@@ -160,23 +160,6 @@ lvim.lsp.installer.setup.ensure_installed = {
 -- }
 --
 
--- local opts = {
--- 	cmd = { 'postgrestools', 'lsp-proxy' },
--- 	filetypes = {
--- 		'sql',
--- 	},
--- 	root_dir = vim.fs.root(0, { 'postgrestools.jsonc' }),
--- 	single_file_support = true,
--- 	docs = {
--- 		description = [[
--- https://pgtools.dev
-
--- A collection of language tools and a Language Server Protocol (LSP) implementation for Postgres, focusing on developer experience and reliable SQL tooling.
---         ]],
--- 	}
--- }
--- require("lvim.lsp.manager").setup("postgres_lsp", opts)
-
 -- ---@usage disable automatic installation of servers
 -- lvim.lsp.installer.setup.automatic_installation = false
 
