ef62fcdb5cf2972505a8c9d607d4b1f03382dd49
carl puchner <carl.puchner@eucalyptus.vc>
2025-03-31 09:39:38 +1100

    unset?h

--- nvim/config.lua
+++ nvim/config.lua
@@ -236,7 +236,6 @@ nvim_lsp.postgres_lsp.setup {
 	cmd = { "postgrestools", "lsp-proxy" },
 	filetypes = { "sql" },
 	root_dir = function(fname)
-		-- TODO: make this automatically build the file based on the repo -> db name
 		return vim.fs.root(fname, { 'postgrestools.jsonc' })
 	end,
 	single_file_support = true,
